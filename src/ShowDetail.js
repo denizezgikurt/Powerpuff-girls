ed82b93e1b039a7ba88e324abbe2a174f044b4a3
Deniz Ezgi Kurt <deniz.ezgi.kurt@epiphany-rbc.com>
2020-09-12 21:29:11 +0200

    API setup with react hook

--- src/ShowDetail.js
+++ src/ShowDetail.js
@@ -1,11 +1,25 @@
 //Home page
 
-import React from 'react';
+import React, { useState, useEffect } from 'react'; //imports useState hook from React
 import { Link } from "@reach/router"
+//Creates links with reach/router, for the episode pages
 
-//Creating links with reach/router, for the episode pages
+async function getData(url = '') {
+    const response = await fetch(url, {
+        method: 'GET',
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+}
 
 function ShowDetail() {
+    const [ showData, setShowData ] = useState(); //setting variables with useState hook
+
+    useEffect(() => {
+        getData('http://api.tvmaze.com/shows/6771').then(data => {
+            setShowData(data)
+        });
+    }, []);
+
     return (
         <div>
             <h1>Title</h1>
