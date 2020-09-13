a8ecef98cc5e4ab55b4850d1d1d6b07a006ddea7
Deniz Ezgi Kurt <deniz.ezgi.kurt@epiphany-rbc.com>
2020-09-13 21:40:04 +0200

    Add styling

--- src/EpisodeDetail.js
+++ src/EpisodeDetail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './EpisodeDetail.css';
 
 function createMarkup(content) {
     return {__html: content};
@@ -6,14 +7,18 @@ function createMarkup(content) {
 
 function EpisodeDetail({ detail, onBack }) {
     return (
-        <div>
-            <button onClick={onBack}>Back</button>
+        <div className="episode-detail">
+            <button className="back-button" onClick={onBack}>GO BACK TO HOMEPAGE</button>
             <h1>{detail.name}</h1>
-            <h4>
-                Season {detail.season} / Episode {detail.number}
-            </h4>
-            <img alt="powerpuff_girls_episodes" src={detail.image.original} width="100%" height="500px" />
-            <div dangerouslySetInnerHTML={createMarkup(detail.summary)} />
+            <div className="primary-content">
+                <img alt="powerpuff_girls_episodes" src={detail.image && detail.image.original} />
+                <div className="detail-wrapper">
+                    <h3>
+                        Season {detail.season} / Episode {detail.number}
+                    </h3>
+                    <div dangerouslySetInnerHTML={createMarkup(detail.summary)} />
+                </div>
+            </div>
         </div>
     )
 }
--- src/ShowDetail.js
+++ src/ShowDetail.js
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 import useApi from './useApi';
 import EpisodeDetail from './EpisodeDetail';
+import './ShowDetail.css';
 
 function createMarkup(content) {
     return {__html: content};
@@ -22,29 +23,36 @@ function ShowDetail() {
         return <EpisodeDetail onBack={() => setEpisodeDetail(false)} detail={episodeDetail} />;
     }
 
+
     return (
-        <div>
+        <div className="show-detail">
             <h1>{data.name}</h1>
-            <img alt="powerpuff_girls_poster" src={data.image.original} width="100%" height="400px" />
-            <div dangerouslySetInnerHTML={createMarkup(data.summary)} />
+            <div className="primary-content">
+                <img alt="powerpuff_girls_poster" src={data.image.original} />
+                <div className="detail-wrapper">
+                    <div dangerouslySetInnerHTML={createMarkup(data.summary)} />
+                    <p><strong>Rating:</strong> {data.rating.average}</p>
+                    <p><strong>Language:</strong> {data.language}</p>
+                    <p><strong>Network:</strong> {data.network.name}</p>
+                </div>
+            </div>
 
-            <p><strong>Rating:</strong> {data.rating.average}</p>
-            <p><strong>Language:</strong> {data.language}</p>
-            <p><strong>Network:</strong> {data.network.name}</p>
-
-            <ul>
-                {
-                    episodes && episodes.map(episode => {
-                        return (
-                            <li key={episode.id}>
-                                <button onClick={() => setEpisodeDetail(episode)}>
-                                    <strong>Episode {episode.number}</strong>: {episode.name}
-                                </button>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+            <div className="episode-list-wrapper">
+                <h3>Episode List</h3>
+                <ul className="episode-list">
+                    {
+                        episodes && episodes.map(episode => {
+                            return (
+                                <li className="episode-list-item" key={episode.id}>
+                                    <button className="episode-link" onClick={() => setEpisodeDetail(episode)}>
+                                        <strong>Episode {episode.number}</strong>: {episode.name}
+                                    </button>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
         </div>
     )
 }
